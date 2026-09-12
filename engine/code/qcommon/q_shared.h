8cb123faef2cff5c2781431d8e8eb267581c4a0d
OrbitaLS2D <53920069+OrbitaLS2D@users.noreply.github.com>
2026-09-12 19:18:59 -0400

    RWA naming: window, baserwa, gameversion

--- engine/code/qcommon/q_shared.h
+++ engine/code/qcommon/q_shared.h
@@ -26,26 +26,26 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // q_shared.h -- included first by ALL program modules.
 // A user mod should never modify this file
 
-#define Q3_VERSION            "Q3 1.32e"
+#define Q3_VERSION            "RWA 0.1"
 #ifndef SVN_VERSION
   #define SVN_VERSION Q3_VERSION
 #endif
-#define CLIENT_WINDOW_TITLE   "Quake 3: Arena"
-#define CONSOLE_WINDOW_TITLE  "Quake 3 Console"
+#define CLIENT_WINDOW_TITLE   "ReQoning Wave Arena"
+#define CONSOLE_WINDOW_TITLE  "RWA Console"
 // 1.32 released 7-10-2002
 
 //#define DEFAULT_GAME			"edawn"
 
-#define BASEGAME				"baseq3"
-#define BASEDEMO				"demoq3"
-#define BASETA					"missionpack"
+#define BASEGAME				"baserwa"
+#define BASEDEMO				"demorwa"
+#define BASETA					"baserwa"
 #define STEAMPATH_NAME			"Quake 3 Arena"
 #define STEAMPATH_APPID			"2200"
 
 #define MAX_TEAMNAME            32
 #define MAX_MASTER_SERVERS      5	// number of supported master servers
 
-#define GAMENAME_FOR_MASTER		"Quake3Arena"
+#define GAMENAME_FOR_MASTER		"ReQoningWaveArena"
 #define HEARTBEAT_FOR_MASTER	"QuakeArena-1"
 
 #define DEMOEXT	"dm_"			// standard demo extension
--- gamecode/code/game/g_local.h
+++ gamecode/code/game/g_local.h
@@ -9,7 +9,7 @@
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"missionpack"
+#define	GAMEVERSION	"baserwa"
 
 #define BODY_QUEUE_SIZE		8
 
